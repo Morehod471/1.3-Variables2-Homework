95858c40e4fee76fbc7f4f308d7c25127f0ba04e
Homework2 <ivliev.k@yandex.ru>
2022-11-30 22:58:20 +0300

    Первый

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        byte a = 1;
+        System.out.println("Значение переменной " + " a " + " с типом " + " byte " + " равно " + a);
+        short b = 1100;
+        System.out.println("Значение переменной " + " b " + " с типом " + " short " + " равно " + b);
+        int camelCase = 235684;
+        System.out.println("Значение переменной " + " camelCase " + " с типом " + " int " + " равно " + camelCase);
+        long d = 1255684L;
+        System.out.println("Значение переменной " + " d " + " с типом " + " long " + " равно " + d);
+        float g = 1.256f;
+        System.out.println("Значение переменной " + " g " + " с типом " + " float " + " равно " + g);
+        double t = 1.5555555555555;
+        System.out.println("Значение переменной " + " t " + " с типом " + " double " + " равно " + t);
     }
 }
